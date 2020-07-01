d8d630653c76b41935863e174a1fa4aa59c84f9b
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-01 08:53:59 +0200

    Installed and imported BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       
-    </div>
+    </Router>
   );
 }
 
