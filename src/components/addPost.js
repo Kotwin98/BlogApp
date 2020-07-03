652494dbc38d91ee96a419fa4e111e6ae552e919
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-03 09:37:43 +0200

    Changed content of addPost.js and styled it

--- src/components/addPost.js
+++ src/components/addPost.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import '../styles/addPost.css';
 
 const addPost = () => (
-    <div>
-        This is addPost page
+    <div className="addPost-container"> 
+        <h1 className="addPost-title">Add post</h1>
+        <form className="addPost-form">
+            <input label="Title" value="Title" className="addPost-input addPost-title-input" />
+            <input label="Author" value="Author" className="addPost-input" />
+            <p className="addPost-par">Write your post content</p>
+            <input className="addPost-content-input" />
+            <button className="addPost-button">Add post</button>
+        </form>
     </div>
 );
 
