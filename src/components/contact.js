3155579800a141470e08ba632717e4403157e045
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-03 07:56:01 +0200

    Changed content of contact.js and added styles to it

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import '../styles/contact.css';
 
 const contact = () => (
-    <div>
-        This is contact page
+    <div className="contact-container">
+        <h1 className="contact-title">Contact</h1>
+        <p className="contact-par1">Contact me any time</p>
+        <p>4293 Long Street</p>
+        <p>San Diego</p>
+        <p className="contact-par1">Mobile Number: 385-293-322</p>
     </div>
 );
 
