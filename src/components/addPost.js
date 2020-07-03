ae4abff75457d14881fec1ff219248ebbd2070e4
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-03 09:46:11 +0200

    Changed addPost into a class based component

--- src/components/addPost.js
+++ src/components/addPost.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/addPost.css';
 
-const addPost = () => (
-    <div className="addPost-container"> 
-        <h1 className="addPost-title">Add post</h1>
-        <form className="addPost-form">
-            <input label="Title" value="Title" className="addPost-input addPost-title-input" />
-            <input label="Author" value="Author" className="addPost-input" />
-            <p className="addPost-par">Write your post content</p>
-            <input className="addPost-content-input" />
-            <button className="addPost-button">Add post</button>
-        </form>
-    </div>
-);
+class AddPost extends Component {
+    render() {
+        return (
+            <div className="addPost-container"> 
+            <h1 className="addPost-title">Add post</h1>
+            <form className="addPost-form">
+                <input label="Title" value="Title" className="addPost-input addPost-title-input" />
+                <input label="Author" value="Author" className="addPost-input" />
+                <p className="addPost-par">Write your post content</p>
+                <input className="addPost-content-input" />
+                <button className="addPost-button">Add post</button>
+            </form>
+            </div>
+        );
+    }
+};
 
-export default addPost;
+export default AddPost;
