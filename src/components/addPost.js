fe58237d59cf8de3bac336280d54ec89cdac7f59
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-03 13:44:50 +0200

    Added onSubmit method to post posts

--- src/components/addPost.js
+++ src/components/addPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import '../styles/addPost.css';
 
 class AddPost extends Component {
@@ -26,15 +27,52 @@ class AddPost extends Component {
         });
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        const post = {
+            title: this.state.title,
+            author: this.state.author,
+            content: this.state.content
+        }
+
+        axios.post('http://localhost:5000/posts/add', post)
+            .then(res => console.log(res.data));
+
+        this.setState({
+            title: '',
+            author: '',
+            content: ''
+        });
+    }
+
     render() {
         return (
             <div className="addPost-container"> 
             <h1 className="addPost-title">Add post</h1>
-            <form className="addPost-form">
-                <input onChange={this.onChangeTitle} label="Title" placeholder="Title" className="addPost-input addPost-title-input" />
-                <input onChange={this.onChangeAuthor} label="Author" placeholder="Author" className="addPost-input" />
+            <form onSubmit={this.onSubmit} className="addPost-form">
+                <input 
+                    type="text"
+                    value={this.state.title}
+                    onChange={this.onChangeTitle} 
+                    label="Title" 
+                    placeholder="Title" 
+                    className="addPost-input addPost-title-input" 
+                />
+                <input 
+                    type="text"
+                    value={this.state.author}
+                    onChange={this.onChangeAuthor} 
+                    label="Author" 
+                    placeholder="Author" 
+                    className="addPost-input" 
+                />
                 <p className="addPost-par">Write your post content</p>
-                <input onChange={this.onChangeContent} className="addPost-content-input" />
+                <input 
+                    type="text" 
+                    value={this.state.content}
+                    onChange={this.onChangeContent} 
+                    className="addPost-content-input" />
                 <button className="addPost-button">Add post</button>
             </form>
             </div>
