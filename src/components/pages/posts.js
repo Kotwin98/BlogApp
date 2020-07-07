c1f09fd9873b5501ee4aca36085376048bb7d4a9
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-07 09:48:56 +0200

    Saved posts data into the state

--- src/components/pages/posts.js
+++ src/components/pages/posts.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/posts/')
+            .then(response => {
+                this.setState({posts: response.data})
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
     render() {
         return (
             <div>
