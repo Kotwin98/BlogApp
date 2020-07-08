2b0cd127c7ee42c2544cf260e8599bfc249ebb58
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-08 11:02:39 +0200

    Add switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navbar from './components/pages/navbar';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/features/NavBar/NavBar';
 import Home from './components/pages/Home/HomePage';
 import AddPost from './components/pages/AddPost/AddPostPage';
 import PostsPage from './components/pages/Posts/PostsPage';
@@ -8,18 +8,22 @@ import Contact from './components/pages/Contact/ContactPage';
 import NotFound from './components/pages/NotFound/NotFoundPage';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <br />
-      <Route path="/" exact component={Home} />
-      <Route path="/add-post" exact component={AddPost} />
-      <Route path="/posts" exact component={PostsPage} />
-      <Route path="/contact" exact component={Contact} />
-      <Route component={NotFound} />
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+          <NavBar />
+          <br />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/add-post" exact component={AddPost} />
+          <Route path="/posts" exact component={PostsPage} />
+          <Route path="/contact" exact component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
