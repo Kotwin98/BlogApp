0c9bf0cbd42b27af5c68a1ef106143b11b1aa82e
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-09 09:22:22 +0200

    Add Button component and styles to it

---
+++ src/components/common/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Button.scss';
+
+const Button = ({ variant = '', children, ...otherProps }) => (
+    <button { ...otherProps } className={ `button button--${variant}`}>
+      { children }
+    </button>
+);
+
+export default Button;
