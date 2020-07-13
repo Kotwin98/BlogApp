e5dc8d40daa7a216fbab6481ed5961cd43a3a310
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-13 14:46:28 +0200

    Change file structure

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,51 +1,32 @@
-// const router = require('express').Router();
-// let Post = require('../models/post.model');
+const router = require('express').Router();
+let Post = require('../models/post.model');
 
-// router.route('/').get((req, res) => {
-//     Post.find()
-//         .then(posts => res.json(posts))
-//         .catch(err => res.status(400).json('Error: ' + err));
-// });
+router.route('/').get((req, res) => {
+    Post.find()
+        .then(posts => res.json(posts))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 
-// router.route('/add').post((req, res) => {
-//     const title = req.body.title;
-//     const author = req.body.author;
-//     const content = req.body.content;
+router.route('/add').post((req, res) => {
+    const title = req.body.title;
+    const author = req.body.author;
+    const content = req.body.content;
 
-//     const newPost = new Post({
-//         title,
-//         author,
-//         content,
-//     });
+    const newPost = new Post({
+        title,
+        author,
+        content,
+    });
 
-//     newPost.save()
-//         .then(() => res.json('Post added!'))
-//         .catch(err => res.status(400).json('Error: ' + err));
-// });
+    newPost.save()
+        .then(() => res.json('Post added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 
-// router.route('/posts/:id').get((req, res) => {
-//     Post.findById(req.params.id)
-//         .then(post => res.json(post))
-//         .catch(err => res.status(400).json('Error: ' + err));
-// });
-
-// module.exports = router;
-
-const express = require('express');
-const router = express.Router();
-
-const PostController = require('../controllers/post.controller');
-
-// get all posts
-router.route('/posts').get(PostController.getPosts);
-
-// add posts
-router.route('/posts').post(PostController.addPost);
-
-// get single post
-router.route('/posts/:id').get(PostController.getSinglePost);
-
-// get posts by range
-router.route('/posts/range/:startAt/:limit').get(PostController.getPostsByRange);
+router.route('/posts/:id').get((req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
