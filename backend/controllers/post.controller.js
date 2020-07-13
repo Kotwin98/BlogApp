59475969a1d342bb4d841b20aaa187d210e3d213
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-13 11:57:08 +0200

    Add addPost function

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -11,3 +11,21 @@ exports.getPosts = async (req, res) => {
     }
 
 };
+
+// add new post
+exports.addPost = async function (req, res) {
+
+    try {
+      const { title, author, content } = req.body;
+  
+      let newPost = new Post(req.body);
+      newPost.id = uuid();
+  
+      postSaved = await newPost.save();
+      res.status(200).json(postSaved);
+  
+    } catch(err) {
+      res.status(500).json(err);
+    }
+  
+};
