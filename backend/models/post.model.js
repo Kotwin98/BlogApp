02da11cfe955d59c00d46410366ea66c94810075
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-13 11:26:12 +0200

    Add id

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
+const Post = new Schema({
+    id: { type: String, required: true },
     title: { type: String, required: true },
     author: { type: String, required: true },
     content: { type: String, required: true },
 });
 
-const Post = mongoose.model('Post', postSchema);
-
-module.exports = Post;
+module.exports = mongoose.model('Post', Post);
