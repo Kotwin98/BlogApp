8eaf31d4550142a0488c2c9d0637200248ebf19c
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-13 14:45:27 +0200

    Change file structure

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const Post = new Schema({
-    id: { type: String, required: true },
+const postSchema = new Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
-    content: { type: String, required: true },
+    content: { type: String, required: true }
 });
 
-module.exports = mongoose.model('Post', Post);
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
