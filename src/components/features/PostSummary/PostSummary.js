3d06db50293b05808714f08bbf3433f27c3b54b3
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-14 08:18:32 +0200

    Update file

--- src/components/features/PostSummary/PostSummary.js
+++ src/components/features/PostSummary/PostSummary.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 import SmallTitle from '../../common/SmallTitle/SmallTitle';
 import HtmlBox from '../../common/HtmlBox/HtmlBox';
-import cutText from '../../../utils/cutText/cutText';
 import './PostSummary.scss';
 
 const postSummary = props => (
     <article className="post-summary">
         <SmallTitle>{props.post.title}</SmallTitle>
         <p>Author: {props.post.author}</p>
-        <HtmlBox>{cutText(props.post.content, 200)}</HtmlBox>
+        <HtmlBox>{props.post.content}</HtmlBox>
     </article>
 );
 
